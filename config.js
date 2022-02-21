8abfbf19f951ae146e3ca57ec11b2b3678a40062
upthera <52497043+upthera@users.noreply.github.com>
2022-02-21 10:59:27 +0100

    Update config.js

--- config.js
+++ config.js
@@ -26,14 +26,14 @@ const CONFIG = {
   bentoLayout: 'buttons', // 'bento', 'lists', 'buttons'
 
   // Weather
-  weatherKey: 'InsertYourAPIKeyHere123456', // Write here your API Key
+  weatherKey: '5dc98b250d34d9c9d7b47bb4d9919d02', // Write here your API Key
   weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
   weatherUnit: 'C', // 'F', 'C'
   language: 'en', // More languages in https://openweathermap.org/current#multi
 
   trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
-  defaultLatitude: '37.775',
-  defaultLongitude: '-122.419',
+  defaultLatitude: '48.208176',
+  defaultLongitude: '16.373819',
 
   // Autochange
   autoChangeTheme: true,
